d962d74b69be132cdcbad4c7e96b0ed8bb6f79ab
kko <kyrylo.kovalenko@pm.me>
2022-09-04 22:34:42 +0300

    Clean Web project

--- SimpleChat.Web/Program.cs
+++ SimpleChat.Web/Program.cs
@@ -4,21 +4,10 @@ using SimpleChat.Web.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
 builder.Services.AddSignalR();
 builder.Services.AddTransient<IUserManager, UserManager>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.MapHub<SimpleChatHub>("/chat");
 
